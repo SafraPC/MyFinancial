e7ee7b6526bc3cdcd1df4f431a3f1ab1f153773b
Leandro Safra <leandrosafra.jconectada@gmail.com>
2024-04-08 09:15:24 -0300

    fix: remove project warnings

--- App.tsx
+++ App.tsx
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './src/styles/theme';
 import { StatusBar } from 'expo-status-bar';
 import AppNavigation from './src/navigation/index';
+import { LogBox } from 'react-native';
+
+const ignoreLogs = [
+   'Non-serializable values were found in the navigation state',
+];
 
 const App = () => {
+   useEffect(() => {
+      LogBox.ignoreLogs(ignoreLogs);
+   }, []);
+
    return (
       <ThemeProvider theme={theme}>
          <StatusBar style="light" />
